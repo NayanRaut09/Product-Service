eea1fa27a2a7374b60caa7321cac51587747d6b2
Nayan Raut <nayanraut@Nayans-Laptop.local>
2026-05-22 08:26:59 +0530

    Adding Product-Service.java code changes

---
+++ Product_Service_Code/src/com/demo/ProductService.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductService {
+
+    public static void main(String[] args) {
+
+
+       List<String> electronicsProducts = Arrays.asList("Samsung washing machine, LG TV, Sony Bravia 3, Iphone 17 pro, LLyod AC") ;
+
+        System.out.println(electronicsProducts);
+
+
+
+
+    }
+
+
+}
