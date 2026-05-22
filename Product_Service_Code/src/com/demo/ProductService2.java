7349854797f9ce48340a89a9636ed99f176d4d8f
Nayan Raut <nayanraut@Nayans-Laptop.local>
2026-05-22 09:04:02 +0530

    resolving PR review comments

--- Product_Service_Code/src/com/demo/ProductService2.java
+++ Product_Service_Code/src/com/demo/ProductService2.java
@@ -8,7 +8,6 @@ public class ProductService2 {
     public static void main(String[] args) {
 
         List<String> homeAppliances =Arrays.asList("Air fryer, Oven , Chimney , LG Refrigerator");
-
         System.out.println(homeAppliances);
     }
 }
