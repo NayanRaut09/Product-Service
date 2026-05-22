f7d17a4ed53ef10a4dfc7103fd994a12568eda10
Nayan Raut <nayanraut@Nayans-Laptop.local>
2026-05-22 08:48:39 +0530

    Adding home appliances prodcuts

---
+++ Product_Service_Code/src/com/demo/ProductService2.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductService2 {
+
+    public static void main(String[] args) {
+
+        List<String> homeAppliances =Arrays.asList("Air fryer, Oven , Chimney , LG Refrigerator");
+
+        System.out.println(homeAppliances);
+    }
+}
